7567dd60c6c0ba4501b9dc0e631a338973d85de3
杜伟 <duwei@MacbookPro-XVI.local>
2018-08-20 22:28:21 +0800

    完成脚本

--- convert.js
+++ convert.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
+const path = require('path')
+const child_process = require('child_process')
 const html = process.argv[2] + '.html'
 
 fs.readFile(html, 'utf-8', function(err, data) {
+  convertLess(data)
   replaceHtml(data)
 })
 
@@ -20,7 +23,15 @@ function replaceHtml(data) {
 
 function convertLess(data) {
   // 批量执行lessc，将less转为css
-  const lesses = data.match(/(?<=\/).*?(?=\.less)/i)
-  console.log(lesses)
+  const lesses = data.match(/(?<=(\/))((?!\/).)*(?=(\.less))/g)
+  lesses.forEach(function(item) {
+    handleLess(item)
+  })
+}
 
+function handleLess(name) {
+  // 执行less命令
+  child_process.execSync(`lessc ${name}.less ${name}.css`, {
+    cwd: path.join(__dirname, 'css')
+  })
 }
