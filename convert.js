a70e9f3c3c092bc5d5d327b182334997702651b8
apple@SMT <helloduwei@hotmail.com>
2018-08-23 15:36:20 +0800

    完善转换脚本

--- convert.js
+++ convert.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
-const html = process.argv[2] + '.html'
+const html = process.argv[2] + '.html' // 获取命令行第一个参数
+const scripts = '<script src="./js/less.js"></script><script>less.watch()</script>' //less脚本字符串
 
 fs.readFile(html, 'utf-8', function(err, data) {
   convertLess(data)
@@ -9,8 +10,8 @@ fs.readFile(html, 'utf-8', function(err, data) {
 })
 
 function replaceHtml(data) {
-  // 将html中的'less'转成'css'
-  const htmlStr = data.replace(/\/\less/g, '').replace(/\less/g, 'css')
+  // 删掉html中的lessjs，并将html中的'less'转成'css'
+  const htmlStr = data.replace(`${scripts}`, '').replace(/\/\less/g, '').replace(/\less/g, 'css')
 
   fs.writeFile(html, htmlStr, function(err) {
     if (err) {
@@ -24,6 +25,10 @@ function replaceHtml(data) {
 function convertLess(data) {
   // 批量执行lessc，将less转为css
   const lesses = data.match(/(?<=(\/))((?!\/).)*(?=(\.less))/g)
+  if (!lesses || lesses.length <= 0) {
+    console.log('no less detected.')
+    return
+  } 
   lesses.forEach(function(item) {
     handleLess(item)
   })
